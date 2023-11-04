d356e0f9bfdbd642756eec68f6c619b0ae6931e4
nanariv <nandhini.ariv@gmail.com>
2023-11-04 12:56:03 -0400

    Project images responsiveness fixed
    Added new game image, media queries added

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -6,7 +6,7 @@ import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
-    <Card className="project-card-view">
+    <Card className={"project-card-view " + props.classAdd}>
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
